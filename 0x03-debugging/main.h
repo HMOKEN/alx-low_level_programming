4fad3623b733ad5337085240eb022a225b86b976
mohammed el fahsi <elfahsiledge@gmail.com>
2023-06-22 10:54:49 -0700

    add

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,25 +1,8 @@
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-}
-root@16391ec7945f:/alx-low_level_programming/0x03-debugging# cat main.h
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
